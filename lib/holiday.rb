320bab8beab0c688a0969ca081837533f85b8200
David McGrann <dpmcgrann@gmail.com>
2018-03-18 17:54:43 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,7 +46,9 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   # code here
   # remember to return the updated hash
   holiday_hash.each do |k, v|
-    v.merge!{k: v}
+    v.merge!{k: "v"}
+    holiday_hash
+  end
 end
 
 def all_winter_holiday_supplies(holiday_hash)
