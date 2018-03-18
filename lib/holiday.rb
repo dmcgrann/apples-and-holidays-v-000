4b6f3567295afc29aa633ed618795003b4bf420b
David McGrann <dpmcgrann@gmail.com>
2018-03-18 18:24:18 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,7 +49,6 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   holiday_hash.each do |season, holiday_name|
     holiday_name.each do |k,v|
       h2.merge({:k => "v"})
-      binding.pry
     end
   end
   h2
