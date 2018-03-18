d2acb76522377b38b78d7d510ae3664fb5395064
David McGrann <dpmcgrann@gmail.com>
2018-03-18 20:00:54 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holiday|
     holiday.map do |holiday, x|
       if x.include?("BBQ")
-        holiday.flatten
+        holiday
       end
     end
   end
