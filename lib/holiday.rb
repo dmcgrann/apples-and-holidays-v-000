e27949a98a8de21d7aad3cf9b3d8a594776561f2
David McGrann <dpmcgrann@gmail.com>
2018-03-18 19:22:08 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -51,6 +51,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  hash = []
   hash = holiday_hash.values.flatten
   hash.split("").capitalize
   hash.join(", ")
